2a5eb829948ea049f70366d00b688186319dfb5c
Rarokko <rarokko@msn.com>
2018-12-20 11:21:54 -0200

    Generated build

---
+++ build/precache-manifest.01cc0350902b7372e275093cba1f9469.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b7d43b9f9a6e175eb04cf83e4b43ba6",
+    "url": "/static/media/ios-icon.1b7d43b9.svg"
+  },
+  {
+    "revision": "d7e67360533b09649bad85420c1f03aa",
+    "url": "/static/media/home-bg.d7e67360.jpg"
+  },
+  {
+    "revision": "83f0e8915de7cdbbe83352fd619f91c3",
+    "url": "/static/media/android-icon.83f0e891.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "46158917d5d1a866833b",
+    "url": "/static/js/main.46158917.chunk.js"
+  },
+  {
+    "revision": "9cba3414f97d5c5fad07",
+    "url": "/static/js/1.9cba3414.chunk.js"
+  },
+  {
+    "revision": "46158917d5d1a866833b",
+    "url": "/static/css/main.40460816.chunk.css"
+  },
+  {
+    "revision": "2e9e95a6203483f954cea89d3f6e2755",
+    "url": "/index.html"
+  }
+];
